7d52ffa7baed314ea4d0ec034a26fd7c37879bbc
Theoden Melgar <theodenmelgar@gmail.com>
2026-01-03 18:39:24 -0800

    completed db setup with postgres container

---
+++ src/main/java/com/theodenmelgar/bracketmanager/BracketManagerApplication.java
@@ -0,0 +1,12 @@
+package com.theodenmelgar.bracketmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BracketManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BracketManagerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/theodenmelgar/bracketmanager/BracketManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theodenmelgar.bracketmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BracketManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
